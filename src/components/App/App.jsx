a06d52708e5aa4e5f70691e653ab38ed4ceb0765
iryna-popkova <131870449+iryna-popkova@users.noreply.github.com>
2023-11-19 15:40:53 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -23,7 +23,7 @@ export class App extends Component {
 
       const initialImages = await fetchImg(this.state.query, this.state.page);
       this.setState({
-        gallery: initialImages,
+        gallery: initialImages.hits,
       });
     } catch (error) {
       this.setState({ error: true });
